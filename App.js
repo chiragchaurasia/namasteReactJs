34037af616dfd64200921068dcdf8e68b741529c
chiragkumar04 <thechiragchaurasia@gmail.com>
2023-11-28 17:50:22 +0530

    ch3-f

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
-// ## Namaste React Course by Akshay Saini
-// # Chapter 02 - Igniting our App
-
 /*
- *** Parcel Feature ***
+ * This are the feature of Parcel
  * Created A Server
  * HMR - Hot Module Replacement
- * File Watcher algorithm - C++ (Execute File when changes occur)
+ * File Watcher algorithm - C++
  * BUNDLING
  * MINIFY
- * Cleaning our Code (Example - Remove Console.log)
- * Dev and Production Build
+ * Cleaning our Code
+ * Dev abd Production Build
  * Super Fast build algorithm
  * Image Optimization
  * Caching while development
  * Compression
  * Compatible with older version of browser
- * HTTPS on dev [Example - npx parcel index.html --https]
- * Port Number [Example - If port number using in localhost then it will change in port number in another project running on localhost]
+ * HTTPS on dev
+ * port Number
  * Consistent Hashing Algorithm
  * Zero Config
  * Tree shaking
@@ -26,32 +23,113 @@
  */
 
 // imported react and reactdom from nodemodule folder
+// import {React, createElement as ce} from 'react';
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "heading"
-);
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "heading1"
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//   },
+//   "heading"
+// );
+// const heading1 = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//   },
+//   "heading1"
+// );
+
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//   },
+//   [heading, heading1]
+// );
+
+// create root using createRoot
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+// root.render(container);
+
+// Create Header element like navbar using createElement
+/*
+ * 
+<div class="header">
+<h1>Title</h1>
+<ul>
+<li>Home</li>
+<li>About Us</li>
+</ul>
+ * 
+ * 
+ */
+// const heading = ce(
+//   "div",
+//   {
+//     className: "header",
+//     key: "header",
+//   },
+//   [ce(
+//     "h1",
+//     {
+//       key: "Title",
+//     },
+//   "Title"), ce(
+//     "ul",
+//     {
+//       key: "ul",
+//     },
+//   [ce(
+//     "li",
+//     {
+//       key: "Home",
+//     },
+//   "Home"),ce(
+//     "li",
+//     {
+//       key: "About Us",
+//     },
+//   "About Us")])]
+// )
+
+// create header element using JSX
+// JSX => React.createElement => Object => HTML (DOM) (babel does all the conversion)
+const heading = (
+  <h1 id="h1" key="h1">
+    This is JSX
+  </h1>
 );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading1]
-);
+// React Component
+// Functional component - new way of writing component
+// Class component - old way of writing component
 
+// Title component is functional component
+const Title = () => {
+  return (
+    <h1 id="title" key="title">
+      Namaste React
+    </h1>
+  );
+};
+// Header component is functional component
+const HeaderComponent = function () {
+  return (
+    <div>
+      <Title />
+      {/* we can also use <Title()> */}
+      {/* we can also use <Title></Title> */}
+      {console.log(10)}
+      <h1>Namaste React Functional component</h1>
+      <h2>This is h2 tag</h2>
+    </div>
+  );
+};
 // create root using createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing react element inside root
-root.render(container);
+root.render(<HeaderComponent />);
